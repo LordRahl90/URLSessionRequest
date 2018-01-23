49e4c5b8e2060c2d6a5a05b36247e657cd40345a
Imaxinacion <tolaabbey001@hotmail.com>
2018-01-23 13:02:52 +0100

    Initial Commit

---
+++ URLSessionRequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  URLSessionRequest
+//
+//  Created by Alugbin LordRahl Abiodun Olutola on 23/01/2018.
+//  Copyright © 2018 Alugbin LordRahl Abiodun Olutola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
